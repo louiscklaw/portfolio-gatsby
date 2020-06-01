96135518f3ca275b462db6978fff44f5fa020d15
louiscklaw <louiscklaw@gmail.com>
2020-06-02 00:43:11 +0800

    feat: update template to use layout,

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,23 +3,27 @@ import { graphql } from "gatsby"
 
 import BackToCatalogue from '../components/back_to_catalogue'
 
+import Layout from '../components/layout'
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container" style={{width: '80%', margin:'auto', paddingTop:'50px'}}>
-      <div className="blog-post">
-        <BackToCatalogue />
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <Layout>
+      <div className="blog-post-container" style={{width: '80%', margin:'auto', paddingTop:'50px'}}>
+        <div className="blog-post">
+          <BackToCatalogue />
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
