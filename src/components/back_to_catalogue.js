e7c11992eb50ff5954c6ea7403980584b59795f2
louiscklaw <louiscklaw@gmail.com>
2020-06-01 23:16:03 +0800

    add working markdown,

---
+++ src/components/back_to_catalogue.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+function BackToCatalogue(){
+  return(
+    <>
+      <Link to="/project-list">Back to catalogue</Link>
+    </>
+  )
+}
+
+export default BackToCatalogue
---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import BackToCatalogue from '../components/back_to_catalogue'
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div className="blog-post-container" style={{width: '80%', margin:'auto', paddingTop:'50px'}}>
+      <div className="blog-post">
+        <BackToCatalogue />
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
