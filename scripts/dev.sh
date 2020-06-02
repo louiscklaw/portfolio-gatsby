b5aad42437a1ac4e18f22f747415622457addf5e
louiscklaw <louiscklaw@gmail.com>
2020-06-02 20:49:01 +0800

    add page,

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,11 +4,7 @@ set -ex
 
 yarn
 
-nodemon -e scss,sass -w src --exec "yarn css-build" &
-nodemon -e scss,sass -w src --exec "yarn css-desktop-build" &
-
-nodemon -e js,css -w src --exec "yarn clean && yarn build" &
-
-serve -s public &
+nodemon -e scss,sass -w src --exec "scripts/build_scss.sh" &
+nodemon -e js,css -w src --exec "yarn start" &
 
 wait
