d8ccd81d652328432749304a67995530f92bccfe
louiscklaw <louiscklaw@gmail.com>
2020-06-02 09:58:47 +0800

    adding husky and robots.txt,

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`
   ],
 }
--- src/components/middle-nav.js
+++ src/components/middle-nav.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Link} from 'gatsby'
 
 import StyleContext from '../contexts/StyleContext'
+import GlobalContext from '../contexts/GlobalContext'
 
 import DesktopMiddleNav from './desktop-middle-nav'
 import MobileMiddleNav from './mobile-middle-nav'
 
 function MiddleNav(){
-  const {target_layout, isMobile} = React.useContext(StyleContext)
+  const {target_layout} = React.useContext(StyleContext)
+  const {isMobile} = React.useContext(GlobalContext)
 
   return(
     <div className={target_layout.middleNav}>
