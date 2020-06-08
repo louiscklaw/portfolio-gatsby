212c2f47f967c9e392e13a7490c33c18a5d6703b
louiscklaw <louiscklaw@gmail.com>
2020-06-08 15:42:26 +0800

    update,

--- src/components/new_window_link.js
+++ src/components/new_window_link.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function NewWindowLink({href, text}){
   return(
-    <div>
+    <span>
       <a href={href} target="_blank" >
         <span>{text ? text : href}</span>
         <i className="fas fa-external-link-alt" style={{marginLeft: '0.2rem'}}></i>
         </a>
-    </div>
+    </span>
   )
 }
 
