3fbe71a5bf2c49cfbbc1009dccba5d6fecd7f25e
louiscklaw <louiscklaw@gmail.com>
2020-06-12 15:40:41 +0800

    update about me,

--- src/components/github_link.js
+++ src/components/github_link.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 function GithubLink(props){
+  let children = props.children
   return(
     <div>
       <a href={`//www.github.com/${props.repo_fullname}`}
         target="_blank" rel="noreferrer"
         >
-        <span>{props.repo_fullname}</span>
+        <span>{ children ? children : props.repo_fullname}</span>
         <i className="fab fa-github" style={{marginLeft: '0.2rem'}} aria-hidden="true" />
         <i className="fas fa-external-link-alt" style={{marginLeft: '0.2rem'}} aria-hidden="true" />
         </a>
