a9fb904465ed285cc350512315c71099db363e4d
louiscklaw <louiscklaw@gmail.com>
2020-06-12 17:45:27 +0800

    update for spacing between link and text,

--- src/components/gist-link.js
+++ src/components/gist-link.js
@@ -7,7 +7,7 @@ function GistLink(props){
   return(
     <div className={style.sourceGistLink}>
       source gist:
-      <a href={`//gist.github.com/${gist_id}`}>
+      <a href={`//gist.github.com/${gist_id}`} style={{paddingLeft: '0.5rem'}}>
         {gist_id}
         <i className="fab fa-github" style={{marginLeft: '0.2rem'}} aria-hidden="true" />
         <i className="fas fa-external-link-alt" style={{marginLeft: '0.2rem'}} aria-hidden="true" />
