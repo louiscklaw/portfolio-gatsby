77cbcd6a4b8ba6c4859f702aa5d98f8aca2f239e
louiscklaw <louiscklaw@gmail.com>
2020-07-20 20:09:46 +0800

    adding tracker_life_cycle,

--- documentations/build.sh
+++ documentations/build.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+touch ./tracker_life_cycle.html
+touch ./qa-process.html
+
 dot -Tsvg ./qa-process.dot > qa-process.svg
+dot -Tsvg ./tracker_life_cycle.dot > tracker_life_cycle.svg
 
 python3 test.py
--- documentations/test.py
+++ documentations/test.py
@@ -3,22 +3,24 @@
 import os,sys
 from pprint import pprint
 
-html_output_path='./qa-process.html'
-html_template_path='./qa-process.html.template'
+qa_process_html_output_path='./qa-process.html'
+tracker_life_cycle_output_path ='./tracker_life_cycle.html'
+
+html_template_path='./svg.html.template'
 
 html_template=''
-
 svg_raw_content=[]
-with open('./qa-process.svg','r') as fi:
-  svg_raw_content = ''.join(fi.readlines()[6:])
 
-with open(html_template_path,'r') as f_template:
-  html_template = ''.join(
-    list(
-      map(lambda x: x.strip(), f_template.readlines())
-    )
-  )
+def genHtml(svg_in, out_html_file_path):
+  with open(svg_in,'r') as fi:
+    svg_raw_content = ''.join(fi.readlines()[6:])
 
-with open(html_output_path,'r+') as fo:
-  fo.truncate(0)
-  fo.writelines(html_template.replace('$SVG_CONTENT',svg_raw_content))
+  with open(html_template_path,'r') as f_template:
+    html_template = ''.join(list(map(lambda x: x.strip(), f_template.readlines())))
+
+  with open(out_html_file_path,'r+') as fo:
+    fo.truncate(0)
+    fo.writelines(html_template.replace('$SVG_CONTENT',svg_raw_content))
+
+genHtml('./qa-process.svg', qa_process_html_output_path)
+genHtml('./tracker_life_cycle.svg', tracker_life_cycle_output_path)
