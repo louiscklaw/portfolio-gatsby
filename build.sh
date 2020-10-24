dd2014be3ced60cfb866415969924132630d9f81
louiscklaw <louiscklaw@gmail.com>
2020-10-24 14:45:13 +0800

    update and test grep readme from other repo,

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 
 set -ex
 
-yarn add global firebase-tools firebase-functions firebase-admin
+# yarn add global firebase-tools firebase-functions firebase-admin
 
 # node scripts/update_build_info.js
 
-yarn
+# grep readme from other repo
+scripts/grep_readme.sh
 
+yarn
+yarn clean
 yarn build
 
 firebase deploy --token $FIREBASE_TOKEN --non-interactive
