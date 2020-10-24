ad7caf1d0432dd027e1021bcc1da3aa4eaefd777
louiscklaw <louiscklaw@gmail.com>
2020-10-24 15:04:35 +0800

    update grep_readme.sh,

--- scripts/grep_readme.sh
+++ scripts/grep_readme.sh
@@ -3,6 +3,6 @@ PROJECT_DETAILS_PATH='src/pages/project-details'
 
 cd $PROJECT_DETAILS_PATH
 
-  wget -O m5stack-playlist.mdx https://raw.githubusercontent.com/louiscklaw/m5stack-playlist/master/README.md
+  wget -O m5stack-playlist.mdx https://raw.githubusercontent.com/louiscklaw/m5stack-playlist/main/README.md
 
 cd -
